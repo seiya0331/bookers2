9b379d217124ecdfc1c4f5a72639967ffd1a49be
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-08-25 15:51:43 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
+  resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/home/about" => "homes#about", as: "about"
